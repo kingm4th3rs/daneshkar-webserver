cb9a9f6c3de85d1bb82e33582a90490cd44ba165
kingm4th3rs <kingm4th3rs@gmail.com>
2026-08-28 13:32:40 -0400

    fix(app): implemented secret validation

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import os
 from fastapi import FastAPI, status
 
 app = FastAPI()
 
+APP_SECRET = os.getenv("APP_SECRET")
 
 @app.get("/", status_code=status.HTTP_200_OK)
 def read_root():
-    return {"msg": "Hello World!"}
+    return {
+        "msg": "Hello World!",
+        "secret_loaded": APP_SECRET is not None 
+    }
 
 @app.get("/health", status_code=status.HTTP_200_OK)
 def health_check():
